4fcba1c0a4483b4ba223a5756ee1dfca0516bf6d
abybells <abbiolbello@yahoo.com>
2022-06-21 09:58:46 -0700

    corrected

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""this is the definition of a square"""
+
+
 class Square:
-    """
-    Creating an empty class that defines a square
-    """
+    """Creating an empty class that defines a square"""
     pass
