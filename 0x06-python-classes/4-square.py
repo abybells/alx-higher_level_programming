8797084091ce79caa923294eb14c8f5f0a473211
abybells <abbiolbello@yahoo.com>
2022-06-21 10:30:49 -0700

    4-square.py added

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""defining a square based on 3-square.py"""
+
+
+class Square:
+    """defines a square by size"""
+    def __init__(self, __size=0):
+        self.__size = __size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (sel.__size * self.__size)
