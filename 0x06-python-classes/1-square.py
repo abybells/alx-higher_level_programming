c55c9babeb81a8d56be16bbe240cfb25ee8a88ab
abybells <abbiolbello@yahoo.com>
2022-06-21 10:01:13 -0700

    1-square added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Define a square and its size"""
+
+
 class Square:
     """
     Add to the class a private instance attribute
     """
-    def __init__(self, __size):
+    def __init__(self, __size=0):
+        """initializing the new square with the size argument"""
         self.__size = __size
