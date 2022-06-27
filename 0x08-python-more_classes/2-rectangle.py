5b47418f0d9cea51c642af6c7c486c022e02714b
abybells <abbiolbello@yahoo.com>
2022-06-27 09:13:17 -0700

    2-rect

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+"""
+
+Rectangle management module
+
+"""
+
+
+
+
+
+class Rectangle:
+
+        """define variables and methods"""
+
+
+
+            def __init__(self, width=0, height=0):
+
+                        """initialize attributes"""
+
+                                self.width = width
+
+                                        self.height = height
+
+
+
+                                            @property
+
+                                                def width(self):
+
+                                                            """getter for width"""
+
+                                                                    return self.__width
+
+
+
+                                                                    @width.setter
+
+                                                                        def width(self, value):
+
+                                                                                    """setter for width"""
+
+                                                                                            if isinstance(value, int) and value >= 0:
+
+                                                                                                            self.__width = value
+
+                                                                                                                    elif not isinstance(value, int):
+
+                                                                                                                                    raise TypeError("width must be an integer")
+
+                                                                                                                                        elif value < 0:
+
+                                                                                                                                                        raise ValueError("width must be >= 0")
+
+
+
+                                                                                                                                                        @property
+
+                                                                                                                                                            def height(self):
+
+                                                                                                                                                                        """getter for height"""
+
+                                                                                                                                                                                return self.__height
+
+
+
+                                                                                                                                                                                @height.setter
+
+                                                                                                                                                                                    def height(self, value):
+
+                                                                                                                                                                                                """setter for height"""
+
+                                                                                                                                                                                                        if isinstance(value, int) and value >= 0:
+
+                                                                                                                                                                                                                        self.__height = value
+
+                                                                                                                                                                                                                                elif not isinstance(value, int):
+
+                                                                                                                                                                                                                                                raise TypeError("height must be an integer")
+
+                                                                                                                                                                                                                                                    elif value < 0:
+
+                                                                                                 :!q
+
