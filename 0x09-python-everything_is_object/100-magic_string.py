729937d562eb2f7223d74ad0ddb80389fc759c74
abybells <99332371+abybells@users.noreply.github.com>
2022-06-28 17:01:35 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
+
 def magic_string():
-    """define magic string"""
-    magic_string.i = getattr(magic_string, 'i', 0) + 1
-    return ', '.join(["BestSchool" for j in range(magic_string.i)])
+    magic_string.counter = getattr(magic_string, "counter", 0) + 1
+    return ", ".join(["Holberton" for i in range(magic_string.counter)])
