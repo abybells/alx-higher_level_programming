7b2a5cd6bb329a7fff70e66d33995a283476b31d
abybells <99332371+abybells@users.noreply.github.com>
2022-07-04 15:59:44 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-class MyList that inherits from list
+module of class MyList that inherits from list
 """
 
 
 class MyList(list):
     """
-    Subclass that inherits from list
+    Subclass that inherits from class list
     """
-
     def print_sorted(self):
-        """print the list, but sorted"""
+        """print the list, but sorted in ascending order"""
         return print(sorted(self))
