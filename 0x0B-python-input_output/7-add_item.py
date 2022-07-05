596d854708b20fcece18646b7b99ca9f0559b247
abybells <99332371+abybells@users.noreply.github.com>
2022-07-05 17:11:30 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,16 +10,16 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 def add_items():
     """
-    function that adds all arguments to a Python list
+    function that adds all arguments to a Json list
     and then saves them to a file
     """
     try:
-        python_list = load_from_json_file('add_item.json')
+        j_list = load_from_json_file('add_item.json')
     except:
-        python_list = []
+        j_list = []
 
     for i in range(1, len(sys.argv)):
-        python_list.append(sys.argv[i])
+        j_list.append(sys.argv[i])
 
     save_to_json_file(python_list, 'add_item.json')
 
