eb1c179e96a571d24930827793d613ca8f5943ff
abybells <99332371+abybells@users.noreply.github.com>
2022-07-05 17:20:56 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 """
-class student
+10-student module
 """
 
 
-class Student():
+class Student:
     """
-    initiate the public instance
+    Define the Student class
     """
+
     def __init__(self, first_name, last_name, age):
+        """initialize variables and methods"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """retrieves dictionary representation of a student"""
-        return self.__dict__
-        comp = 0
-
-        if type(attrs) == list:
-            for item in attrs:
-                if type(item) == str:
-                    comp += 1
-            if len(attrs) == comp:
-                n_dict = {}
-                for item in self_dict.keys():
-                    if item in attrs:
-                        n_dict[item] = self_dict[item]
-                    return n_dict
-                else:
-                    return self_dict
+    def to_json(self, attrs=None):
+        """
+        function that retrieves a dictionary representation
+        of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            a_dict = {}
+            for attr, value in (self.__dict__).items():
+                if attr in attrs:
+                    a_dict[attr] = value
+            return a_dict
