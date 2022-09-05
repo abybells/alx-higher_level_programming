9db53503b763cde3a5b64cf4d08b9a7b8032ec6e
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-09-05 18:28:34 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
 function add (a, b) {
-  return (a + b);
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  }
+  return a + b;
 }
-
-if (isNaN(myArgs[0]) || isNaN(myArgs[1])) {
-  console.log('NaN');
-} else {
-  console.log(add(parseInt(myArgs[0]), parseInt(myArgs[1])));
-}
+console.log(add(first, second));
