4ae14e0905a09ce9d857b2f169ff2e8b81ba2f95
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-09-05 18:32:46 +0100

    Create 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,12 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
+let args = process.argv.slice(2).map((x) => {
+  return parseInt(x);
+});
 
-function secondMax (a) {
-  if (a.length === 0 || a.length === 1) {
-    console.log(0);
-  } else {
-    const varPop = Math.max(...a);
-    const aCopy = [];
-    for (let i = 0; i < a.length; i++) {
-      if (parseInt(a[i]) !== varPop) {
-        aCopy.push(a[i]);
-      }
-    }
-    console.log(Math.max(...aCopy));
-  }
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  console.log(args.sort((a, b) => {
+    return b - a;
+  })[1]);
 }
-
-secondMax(myArgs);
