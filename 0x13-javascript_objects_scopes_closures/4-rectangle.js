c0e2ffad02beb907f4f28fbdc0c2342938889a7d
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-09-06 17:47:39 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -22,14 +22,14 @@ class Rectangle {
 
   rotate () {
     let temp;
-    temp = this.height;
-    this.height = this.width;
-    this.width = temp;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
 
   double () {
-    this.height *= 2;
-    this.width *= 2;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 }
 
