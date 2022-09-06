8d89a32ef32b2e81fb78e4cac4ef03c7c14bc8d5
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-09-06 17:56:35 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-// function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-
-  let rlist = [];
-  while (list.length > 0) {
-    rlist.push(list.pop());
-  }
-  return rlist;
+  const listCopy = [];
+  list.forEach(element => listCopy.unshift(element));
+  return listCopy;
 };
