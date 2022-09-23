ea2bd47b789fe527507afc4f8a4085a622032f5a
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-09-23 13:02:05 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
 Python script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variable found in the header of the response
+the value of the variable X-Request-Id in the response header
 """
-if __name__ == "__main__":
+import requests
+from sys import argv
 
-    import requests
-    import sys
-
-    response = requests.get(sys.argv[1])
-    print(response.headers.get('X-Request-Id'))
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
