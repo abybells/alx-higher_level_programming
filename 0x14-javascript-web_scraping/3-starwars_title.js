191ed6b296d3bf0a0f32c06864829c9317077872
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-18 14:10:25 +0100

    print title where episode = int

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// prints the title of a star wars movie where episode no.
+// matches given integer
+
+const arg = process.argv;
+const movieId = arg[2]
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
