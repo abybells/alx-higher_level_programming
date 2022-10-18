8d5c88fd6eb27830da631f92f342a0a07933b003
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-18 16:11:55 +0100

    list number of tasks completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id
+
+const args = process.argv;
+const requestURL = args[2];
+const request = require('request');
+
+request(requesturl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const listTasks = JSON.parse(body);
+    const obJs = {};
+    for (let i = 0; i < listTasks.length; i++) {
+      if (listTasks[i].completed === true) {
+        if (obJs[listTasks[i].userId] === undefined) {
+          obJs[listTasks[i].userId] = 0;
+        }
+        obJs[listTasks[i].userId] += 1;
+      }
+    }
+    console.log(obJs);
+  }
+});
