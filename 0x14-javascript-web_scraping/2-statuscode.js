5b5814799f51ab1f5a73ba15d973e6edb25163b4
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-18 17:21:37 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // script that displays the status code of a GET request
 // first argument is the URl to GET
-// print status code as: code: <status code>
-// Use module request
 
 const arg = process.argv;
 const requestURL = arg[2];
 const request = require('request');
 
-req.get(requestURL).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+request.get(requestURL).on('response', function (eroor, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else console.log('code:', response && response.statusCode);
 });
