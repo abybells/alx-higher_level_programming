162227210c3831d0867a443c879f1eba4b3ce3a1
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-18 15:48:53 +0100

    movie count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 // prints the number of movies where the character Wedge Antilles is present
 
-const args = process.argv;
-const URLreq = args[2];
+const arg = process.argv;
+const requestURL = arg[2];
 const request = require('request');
 
-request(URLreq, function (error, response, body) {
+request(requestURL, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     let listFilms = (JSON.parse(body).results);
     let movieCount = 0;
     for (let i = 0; i < listFilms.length; i++) {
-      let chars = (listFilms[i]['characters']);
-      for (let j = 0; j < chars.length; j++) {
-        let check18 = chars[j].endsWith('18/');
-        if (check18) {
-          movieCount++;
+      for (let j = 0; j < listFilms[i].characters.length; j++) {
+        if (listFilms[i].characters[j].includes('/18/')) {
+          movieCount += 1;
         }
       }
     }
