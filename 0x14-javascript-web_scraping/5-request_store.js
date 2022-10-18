ff07c4213f6306ce0362dfea16ea5db4e66db959
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-18 14:45:25 +0100

    get content and store in file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// script gets contents of a webpage and stores it in a file
+
+const args = process.argv;
+const requestURL = args[2];
+const fPath = args[3];
+const fs = require('fs');
+const req = require('request');
+
+req(requestURL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(fPath, body, 'utf8', function (error) {
+      if (error) { console.log(error); }
+    });
+  }
+});
