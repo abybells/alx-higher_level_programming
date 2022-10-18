93a1638f3fa1d2f9e16eda04c4b6d799f84aac61
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-18 13:02:23 +0100

    read file content

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,9 +8,9 @@ const myfile = process.argv;
 const fs = require('fs');
 
 fs.readFile(myfile[2], 'utf8', function (err, data) {
-if (err) {
-console.log(err);
-} else {
-console.log(data.toString());
-}
+  if (err) {
+    console.log(err);
+    } else {
+      console.log(data.toString());
+      }
 });
