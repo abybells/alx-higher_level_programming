8239408db14dbbd604f3bed805e2aa3ca555d318
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-18 17:23:40 +0100

    get status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,12 +2,10 @@
 // script that displays the status code of a GET request
 // first argument is the URl to GET
 
+
 const arg = process.argv;
 const requestURL = arg[2];
-const request = require('request');
-
-request.get(requestURL).on('response', function (eroor, response, body) {
-  if (error) {
-    console.log('error:', error);
-  } else console.log('code:', response && response.statusCode);
+const req = require('request');
+req.get(requestURL).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
