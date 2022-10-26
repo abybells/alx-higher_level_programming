1e84177aa44a9f4d7971975b8296f8e18ec5d412
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-26 22:44:07 +0100

    use language api

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,11 @@
+// say hello in diffferent languages
+const url = 'https://fourtonfish.com/hellosalut/?lang=';
+document.addEventListener('DOMContentLoaded', function () {
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $('INPUT#language_code').val(), function (data, status) {
+      if (status === 'success') {
+        $('DIV#hello').text(data.hello);
+      }
+    });
+  });
+});
